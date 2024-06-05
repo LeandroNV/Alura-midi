bf6dd77cd2856e6d1929cf6d7993666a987297a2
leonicvarc <leonicvarc@gmail.com>
2024-06-05 22:10:22 +0000

    Pt3

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 while (contador < 9) {
-    listaDeTeclas[contador].onclick = function () {
-        playSonido('#sonido_tecla_pom')
-    };
-    contador++;
-    console.log('Vuelta' + contador);
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#sonido_${instrumento}`;
+  tecla.onclick = function () {
+    playSonido(idAudio);
+  };
+  contador++;
 }
+
